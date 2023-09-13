0ed6187112428dd7210acb5c872223c8f0042359
Akila <akilasekar1997@gmail.com>
2023-09-13 19:46:27 +0530

    commit

--- Day25_task/my-app/src/App.jsx
+++ Day25_task/my-app/src/App.jsx
@@ -4,58 +4,10 @@ import Todo from './Todo'
 
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [inputValue1, setInputValue1] = useState('');
-  const [submittedValue, setSubmittedValue] = useState('');
-  const [submittedValue1, setSubmittedValue1] = useState('');
-  const [showComponent, setShowComponent] = useState(false);
-  const cardsData={submittedValue,submittedValue1};
-
-  const handleInputChange = event => {
-    setInputValue(event.target.value);
-  };
-
-  
-  const handleInputChange1 = event => {
-    setInputValue1(event.target.value);
-  };
-
-  const handleSubmit = event => {
-     event.preventDefault();
-    setSubmittedValue(inputValue);
-    setSubmittedValue1(inputValue1);
-    setShowComponent(true);
-  };
-  
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input className='element1'
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Todo Name"
-        />
-        <input className='element1'
-          type="text"
-          value={inputValue1}
-          onChange={handleInputChange1}
-          placeholder="Todo Description"
-        />
-        <button type="submit">Submit</button>
-        {/* {showComponent && <Todo submittedValue={submittedValue} submittedValue1={submittedValue1} />} */}
-        
-        {showComponent && (
-        <div className="card-list">
-          {cardsData.map(card => (
-           <Todo submittedValue={submittedValue} submittedValue1={submittedValue1} />
-          ))}
-        </div>
-      )}
-
-      </form>
       
+      <Todo/>
     </div>
   )
 }
--- Day25_task/my-app/src/Todo.jsx
+++ Day25_task/my-app/src/Todo.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
 
-function Todo(props) {
+function Todo() {
   return (
-    <div>
-       <label  className="element">My Todos</label>
-      <label  className="element">Status Filter</label>
-      <div className="card">
-    <h2 className="card-title">Todo</h2>
-    <div className="card-content">
-      <p><label>Name:</label>{props.submittedValue}</p>
-      <p><label>Description:</label>{props.submittedValue1}</p>
-      <button className="element1">Edit</button><button className="element1">Delete</button>
-    </div>
-  </div>
-  </div>
+    <div>Todo</div>
   )
 }
 
